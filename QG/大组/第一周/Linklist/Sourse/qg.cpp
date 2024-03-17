0c687d49b7824bdba7dbace886252ee01945e214
chenwenwan-git <156827164+chenwenwan-git@users.noreply.github.com>
2024-03-17 17:49:11 +0800

    第一周作业

---
+++ QG/大组/第一周/Linklist/Sourse/qg.cpp
@@ -0,0 +1,121 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+// 定义链表节点结构体
+typedef struct LNode {
+    int data; // 数据域
+    struct LNode* next; // 指针域，指向下一个节点
+} LNode, * LinkedList;
+
+// 函数声明
+void CreateLinkedList(LinkedList* L);
+void InsertLinkedList(LinkedList L, int position, int value);
+void DeleteLinkedList(LinkedList* L, int position);
+void DestroyLinkedList(LinkedList* L);
+void PrintLinkedList(LinkedList L);
+
+int main() {
+    // 创建链表
+    LinkedList L;
+    CreateLinkedList(&L);
+
+    // 向链表插入数据
+    InsertLinkedList(L, 1, 1);
+    InsertLinkedList(L, 2, 2);
+    InsertLinkedList(L, 3, 3);
+
+    // 打印链表
+    PrintLinkedList(L);
+
+    // 删除链表中的节点
+    DeleteLinkedList(&L, 2);
+
+    // 再次打印链表
+    PrintLinkedList(L);
+
+    // 销毁链表
+    DestroyLinkedList(&L);
+
+    return 0;
+}
+
+// 创建链表函数实现
+void CreateLinkedList(LinkedList* L) {
+    *L = (LNode*)malloc(sizeof(LNode)); // 创建头结点
+    (*L)->next = NULL; // 初始化头结点的指针域为NULL
+}
+
+// 插入节点函数实现
+void InsertLinkedList(LinkedList L, int position, int value) {
+    int i;
+    LNode* p = L; // p指向头结点
+    LNode* current;
+
+    // 寻找插入位置的前一个节点
+    for (i = 1; p != NULL && i < position; i++) {
+        p = p->next;
+    }
+
+    // 检查position是否有效
+    if (p == NULL) {
+        printf("Position %d is out of bounds.\n", position);
+        return;
+    }
+
+    // 创建新节点
+    current = (LNode*)malloc(sizeof(LNode));
+    current->data = value; // 设置新节点的数据
+    current->next = p->next; // 将新节点的指针域指向前一个节点的下一个节点
+    p->next = current; // 更新前一个节点的指针域，指向新节点
+}
+
+// 删除节点函数实现
+void DeleteLinkedList(LinkedList* L, int position) {
+    int i;
+    LNode* p = *L; // p指向头结点
+    LNode* temp;
+
+    // 寻找要删除的节点的前一个节点
+    for (i = 1; p != NULL && i < position; i++) {
+        p = p->next;
+    }
+
+    // 检查position是否有效
+    if (p == NULL) {
+        printf("Position %d is out of bounds.\n", position);
+        return;
+    }
+
+    // 删除节点
+    temp = p->next;
+    p->next = temp->next;
+    free(temp); // 释放被删除节点的内存
+}
+
+// 销毁链表函数实现
+void DestroyLinkedList(LinkedList* L) {
+    LNode* p = *L;
+    while (p != NULL) {
+        LNode* temp = p;
+        p = p->next;
+        free(temp);
+    }
+    *L = NULL;
+    printf("The linked list has been destroyed.\n");
+}
+
+// 打印链表函数实现
+void PrintLinkedList(LinkedList L) {
+    LNode* p = L->next; // 从头结点的下一个节点开始遍历
+    if (p == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+
+    printf("Linked List (head to tail): ");
+    while (p != NULL) {
+        printf("%d -> ", p->data);
+        p = p->next;
+    }
+    printf("NULL\n");
+}
---
+++ QG/大组/第一周/Linklist/Sourse/qg2.cpp
@@ -0,0 +1,146 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+// 创建结构体
+typedef struct Node {
+    int data;//数据域
+    struct Node* prev;// 前驱节点指针
+    struct Node* next;// 后继节点指针
+} Node;
+
+
+typedef struct DoublyLinkedList {
+    int size;
+    Node* front;
+    Node* tail;
+} DoublyLinkedList;
+
+// 创建函数
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+DoublyLinkedList* creatList() {
+    DoublyLinkedList* list = (DoublyLinkedList*)malloc(sizeof(DoublyLinkedList));
+    list->size = 0;
+    list->front = list->tail = NULL;
+    return list;
+}
+
+
+
+
+// 插入头节点
+void insertAtBeginning(DoublyLinkedList* list, int data) {
+    Node* newNode = createNode(data);
+    if (list->front == NULL) {
+        list->front = newNode;
+        list->tail = newNode;
+    }
+    else {
+        list->front->prev = newNode;
+        newNode->next = list->front;
+    }
+    list->front = newNode;
+    list->size++;
+    
+}
+
+void insertAtEnd(DoublyLinkedList* list, int data) {
+    Node* newNode = createNode(data);
+    if (list->tail == NULL) {
+        list->front = newNode;
+        
+    }
+    else {
+        list->tail->next = newNode;
+        newNode->prev = list->tail;
+    }
+    list->tail = newNode;
+    list->size++;
+}
+
+void insertAtPos(DoublyLinkedList* list, int data, int posdata) {
+    if (list->size == 0) {
+        printf("The list is empty.\n");
+        return;
+    }
+    else if (list->front->data == posdata) {
+        insertAtBeginning(list, data);
+    }
+    else {
+        Node* posNode = list->front->next;
+        Node* posNodeLeft = list->front;
+        while (posNode != NULL && posNode->data != posdata) {
+            posNodeLeft = posNode;
+            posNode = posNode->next;
+
+        }
+        if (posNode == NULL) {
+            printf("fail founded!\n");
+            return;
+        }
+        else {
+            Node* newNode = createNode(data);
+            posNodeLeft->next = newNode;
+            newNode->prev = posNodeLeft;
+            newNode->next = posNode;
+            posNode->prev = newNode;
+        }
+    }
+}
+
+
+// 打印函数
+void displayByleft(DoublyLinkedList* list) {
+    if (list->size == 0) {
+        printf("The list is empty.\n");
+    }
+    else {
+        Node* current = list->tail;
+        while (current) {
+
+            printf("%d\t", current->data);
+            current = current->prev;
+        }
+    }
+    printf("\n");
+}
+
+void displayByright(DoublyLinkedList* list) {
+    if (list->size == 0) {
+        printf("The list is empty.\n");
+    }
+    else {
+        Node* current = list->front;
+        while (current) {
+
+            printf("%d\t", current->data);
+            current = current->next;
+        }
+    }
+    printf("\n");
+}
+
+
+int main() {
+    DoublyLinkedList* list = creatList();
+
+    insertAtBeginning(list, 3);
+    insertAtBeginning(list, 2);
+    insertAtBeginning(list, 1);
+    insertAtPos(list, 200, 1);
+    displayByleft(list);
+    displayByright(list);
+    insertAtEnd(list, 100);
+    displayByleft(list);
+    displayByright(list);
+    
+
+    return 0;
+}
